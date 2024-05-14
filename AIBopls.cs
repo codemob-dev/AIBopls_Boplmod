72e9d7fcd78590860911ec57beb3a174b3004460
codemob <69110900+commandblox@users.noreply.github.com>
2024-05-14 11:32:09 -0400

    Basic randomized movement

--- AIBopls.cs
+++ AIBopls.cs
@@ -1,6 +1,6 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using System;
+using UnityEngine;
 
 namespace AIBopls
 {
@@ -8,16 +8,18 @@ namespace AIBopls
     public class AIBopls : BaseUnityPlugin
     {
         public Harmony harmony;
-        public InputOverrides inputOverrides = new InputOverrides();
+        public static InputOverrides inputOverrides = new InputOverrides();
+        public static AIBopls instance;
         private void Awake()
         {
             harmony = new Harmony(Info.Metadata.GUID);
             harmony.PatchAll(typeof(AIBopls));
+            instance = this;
         }
 
         [HarmonyPatch(typeof(Player), nameof(Player.ForceSetInputProfile))]
         [HarmonyPrefix]
-        public void Player_ForceSetInputProfile(ref Player __instance,
+        public static void Player_ForceSetInputProfile(ref Player __instance,
                                                 ref bool startDown, 
                                                 ref bool selectDown, 
                                                 ref bool jumpDown, 
@@ -32,7 +34,7 @@ namespace AIBopls
         {
             if (GameLobby.isPlayingAReplay) return;
             if ((GameLobby.isOnlineGame && __instance.IsLocalPlayer) 
-                || (!GameLobby.isOnlineGame && __instance.Id == 0))
+                || (!GameLobby.isOnlineGame && __instance.Id == 1))
             {
                 startDown = inputOverrides.startDown;
                 selectDown = inputOverrides.selectDown;
@@ -46,8 +48,26 @@ namespace AIBopls
                 s = inputOverrides.s;
                 d = inputOverrides.d;
 
+            } else
+            {
+                instance.Logger.LogInfo(__instance.Id);
             }
         }
+
+        [HarmonyPatch(typeof(PlayerBody), nameof(PlayerBody.UpdateSim))]
+        [HarmonyPrefix]
+        public static void PlayerBody_UpdateSim(PlayerBody __instance)
+        {
+            Player player = PlayerHandler.Get().GetPlayer(__instance.GetComponent<IPlayerIdHolder>().GetPlayerId());
+            if ((GameLobby.isOnlineGame && player.IsLocalPlayer)
+                || (!GameLobby.isOnlineGame && player.Id == 1)) {
+                if (Random.Range(0f, 1f) > .95)
+                {
+                    inputOverrides.SetMovementFromVector(Random.insideUnitCircle);
+                }
+            }
+        }
+
         public struct InputOverrides
         {
             public bool startDown;
@@ -61,6 +81,15 @@ namespace AIBopls
             public bool a;
             public bool s;
             public bool d;
+
+            public void SetMovementFromVector(Vector2 vector)
+            {
+                vector.Normalize();
+                w = vector.y > .5;
+                a = vector.x < -.5;
+                s = vector.y < -.5;
+                d = vector.x > .5;
+            }
         }
     }
 }
