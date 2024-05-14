b7aa3cc22316e99cd2cec613f1612fbddcd96c81
codemob <69110900+commandblox@users.noreply.github.com>
2024-05-13 20:23:54 -0400

    Add project files.

---
+++ AIBopls.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine.Assertions;
+
+namespace AIBopls
+{
+    [BepInPlugin("com.codemob.aibopls", "AI Bopls", "1.0.0")]
+    public class AIBopls : BaseUnityPlugin
+    {
+        public Harmony harmony;
+        private void Awake()
+        {
+            harmony = new Harmony(Info.Metadata.GUID);
+            harmony.PatchAll(typeof(AIBopls));
+        }
+    }
+}
